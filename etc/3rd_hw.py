75fa1ec5522c7cbe3e8f3fc381ccab4127f0e0f9
heydome <64726772+heydome@users.noreply.github.com>
2020-06-01 17:42:50 +0900

    Update 3rd_hw.py

--- etc/3rd_hw.py
+++ etc/3rd_hw.py
@@ -1,6 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 
+from pymongo import MongoClient           # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+db = client.dbsparta                      # 'dbsparta'라는 이름의 db를 만듭니다.
+
 # URL을 읽어서 HTML를 받아오고,
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&ymd=20200403&hh=23&rtm=N&pg=1',headers=headers)
@@ -21,4 +25,10 @@ for song in songs:
         rank = song.select_one('td.number').text[0:3].strip()
         title = a_tag.text.strip()                                    
         name = song.select_one('a.artist.ellipsis').text                # td 태그 사이의 텍스트를 가져오기
-        print(rank,title,name)
+        db.songs.insert({'rank':rank,'name':name,'title':title})
+        
+        # print(rank,title,name)
+    print_song = list(db.songs.find())
+# print(print_song)
+for p_song in print_song:
+    print(p_song['rank'],p_song['title'],p_song['name'])
